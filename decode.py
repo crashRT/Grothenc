4755b9d2d51240efd4fcbd666c045442f06fa718
crashRT <85057127+crashRT@users.noreply.github.com>
2022-11-10 18:16:56 +0900

    decode

--- decode.py
+++ decode.py
@@ -3,10 +3,14 @@ from secret_key import *
 
 
 def grothdec(cipher):
-
     # decode
-    hex = bytes.fromhex(cipher)
-    int_cipher = int.from_bytes(hex, 'big')
-    message = ((int_cipher, 'big') ** d) % 57
-    message = message.decode('UTF-8')
+    string = cipher.lower()
+    hex = string.split(' ')
+    message = []
+    for number in hex:
+        number = bytes.fromhex(number)
+        number = int.from_bytes(number, 'big')
+        m = (number ** d) % 57
+        m = m.to_bytes(1, 'big')
+        message.append(m.decode('UTF-8'))
     return message
--- decode_test.py
+++ decode_test.py
@@ -1,6 +1,7 @@
 from decode import *
 
-source = input("enter cipher: ")
+# source = input("enter cipher: ")
+source = "05 09 1F 11"
 
 print("----------")
 
