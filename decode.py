d0cd6b637c8e717222f3b0b0b5d2f620bdd14905
crashRT <crashrt.farbauti@gmail.com>
2023-03-26 16:59:35 +0900

    decode 完成

--- decode.py
+++ decode.py
@@ -3,23 +3,13 @@ from secret_key import *
 
 
 def grothdec(cipher):
-    # decode
-    string = cipher.lower()
-    hex = string.split(' ')
-    message = []
-    for char in hex:
-        m = []
-        print(char)
-        number = bytes.fromhex(char)
-        number = int.from_bytes(number, 'big')
-        for i in range(2):
-            if (i == 0):
-                num = number // 16
-            else:
-                num = number % 16
-            m.append((num ** d) % n)
-        text = m[0] * 16 + m[1]
-        print(text)
-        text = text.to_bytes(1, 'big')
-        message.append(text.decode('UTF-8'))
-    return message
+    '''
+    57を用いてRSA復号化を行う
+    '''
+
+    source = cipher.split()
+    m = []
+    for text in source:
+        m.append((int(text) ** d) % n)
+    hex = "".join(list(map(lambda x: "%1X" % x, m)))
+    return bytes.fromhex(hex).decode('UTF-8')
